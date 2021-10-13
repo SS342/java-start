7d9a2eb45c384b44f391ca955177ab964a051e23
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2021-10-13 13:50:52 +0300

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,20 @@
+package com.max.idea;
+
+import java.util.*;
+
+class Main{
+    public static void main(String[] argments){
+
+        System.out.println("Your weight omn earth is ");
+        double weight = 178;
+
+        System.out.println(weight);
+
+        System.out.println("Your weight in Mercury is ");
+        double mercury = weight * .378;
+
+        System.out.println(mercury);
+        
+        
+    }
+}
