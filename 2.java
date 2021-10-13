e548fbc44dc31f5cfddb5c9855c0309c7210edaa
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2021-10-13 09:42:18 +0300

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,13 @@
+package com.max.idea;
+
+class Main{
+    public static void main(String[] argments){
+
+        int number = 225;
+
+        System.out.println("The square root of "
+            + number
+            + " is "
+            + Math.sqrt(number));
+    }
+}
