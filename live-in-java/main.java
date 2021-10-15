3ac8faec6a9c5c6113cdd583da911c1a0a074d63
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2021-10-15 13:05:57 +0300

    Create main.java

---
+++ live-in-java/main.java
@@ -0,0 +1,12 @@
+package com.max.idea;
+
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+
+        Window window = new Window();
+        javax.swing.SwingUtilities.invokeLater(window);
+
+    }
+}
